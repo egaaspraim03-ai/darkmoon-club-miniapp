633749456bf6f2624c05c51dd65bcfaf33544c02
egaaspraim03-ai <egaaspraim03@gmail.com>
2026-03-29 13:11:26 +0300

    Update pokedex-data.js

--- pokedex-data.js
+++ pokedex-data.js
@@ -1,12 +1,11 @@
 const pokedexData = [
   { num: "0001", ru: "Бульбазавр", sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" },
+  { num: "0002", ru: "Айвизавр", sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png" },
+  { num: "0003", ru: "Венузавр", sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png" },
   { num: "0004", ru: "Чармандер", sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png" },
-  { num: "0007", ru: "Сквиртл", sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png" },
-  { num: "0025", ru: "Пикачу", sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" },
-  { num: "0039", ru: "Джигглипафф", sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/39.png" },
-  { num: "0066", ru: "Мачоп", sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/66.png" },
-  { num: "0143", ru: "Снорлакс", sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/143.png" }
-  // Добавляй сюда сколько угодно покемонов в таком же формате
+  { num: "0005", ru: "Чармелеон", sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png" },
+  { num: "0006", ru: "Чаризард", sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png" },
+  { num: "0007", ru: "Снорлакс", sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png" }
 ];
 
 console.log("✅ pokedex-data.js загружен — " + pokedexData.length + " покемонов");
