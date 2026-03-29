76fbd61b2819da0abee693068d142c284949ad99
egaaspraim03-ai <egaaspraim03@gmail.com>
2026-03-29 17:01:46 +0300

    Update smeshariki-data.js

--- smeshariki-data.js
+++ smeshariki-data.js
@@ -3,15 +3,15 @@
 
 const smesharikiData = [
   { num: "S001", ru: "Крош",     sprite: "https://i.postimg.cc/WzCLZhcb/IMG-20260329-160601-701.png" },
-  { num: "S002", ru: "Ёжик",    sprite: "https://i.imgur.com/5YkZ3vL.png" },
-  { num: "S003", ru: "Пин",      sprite: "https://i.imgur.com/3YkZ3vL.png" },
-  { num: "S004", ru: "Лосяш",    sprite: "https://i.imgur.com/7YkZ3vL.png" },
-  { num: "S005", ru: "Кар-Карыч", sprite: "https://i.imgur.com/9YkZ3vL.png" },
-  { num: "S006", ru: "Совунья",  sprite: "https://i.imgur.com/1YkZ3vL.png" },
-  { num: "S007", ru: "Бараш",    sprite: "https://i.imgur.com/2YkZ3vL.png" },
-  { num: "S008", ru: "Копатыч",  sprite: "https://i.imgur.com/4YkZ3vL.png" },
-  { num: "S009", ru: "Нюша",     sprite: "https://i.imgur.com/6YkZ3vL.png" },
-  { num: "S010", ru: "Ромашка",  sprite: "https://i.imgur.com/8YkZ3vL.png" }
+  { num: "S002", ru: "Ёжик",    sprite: "https://i.postimg.cc/02C793Dm/IMG-20260329-160602-004-jpg.png" },
+  { num: "S003", ru: "Пин",      sprite: "https://i.postimg.cc/5ygjWhGZ/1774791142293.png" },
+  { num: "S004", ru: "Лосяш",    sprite: "https://i.postimg.cc/PrdTm8DX/IMG-20260329-160602-181-jpg.png" },
+  { num: "S005", ru: "Кар-Карыч", sprite: "https://i.postimg.cc/3Nv9LZcW/IMG-20260329-160602-421-jpg.png" },
+  { num: "S006", ru: "Совунья",  sprite: "https://i.postimg.cc/hvpw8hFr/IMG-20260329-160602-009-jpg.png" },
+  { num: "S007", ru: "Бараш",    sprite: "https://i.postimg.cc/BQz7JrJX/IMG-20260329-160602-535-jpg.png" },
+  { num: "S008", ru: "Копатыч",  sprite: "https://i.postimg.cc/nVgqrp9Q/IMG-20260329-160601-887-jpg.png" },
+  { num: "S009", ru: "Нюша",     sprite: "https://i.postimg.cc/Jh0hjH9b/IMG-20260329-160601-732-jpg.png" },
+  { num: "S010", ru: "Би-Би",  sprite: "https://i.postimg.cc/c1krTSRY/image-1467-jpg.png }
 ];
 
 console.log(`✅ smeshariki-data.js загружен — ${smesharikiData.length} персонажей Смешариков`);
