8da0e94258a40a13e9669b49b6ec618183534390
egaaspraim03-ai <egaaspraim03@gmail.com>
2026-03-31 03:15:53 +0300

    Update overworld.js

--- overworld.js
+++ overworld.js
@@ -1,4 +1,3 @@
-// overworld.js
 let phaserGame = null, currentZone = null, playerGridX = 5, playerGridY = 5, playerSprite = null;
 const mapData = [[1,1,1,1,1,1,1,1,1,1],[1,0,0,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,0,0,1],[1,0,1,1,0,0,1,0,0,1],[1,0,0,0,0,1,0,0,0,1],[1,0,0,0,0,0,0,1,0,1],[1,0,1,0,0,0,0,0,0,1],[1,0,0,0,1,0,0,0,0,1],[1,0,0,0,0,0,0,0,0,1],[1,1,1,1,1,1,1,1,1,1]];
 
@@ -9,12 +8,14 @@ class OverworldScene extends Phaser.Scene {
     this.load.image('player', first.sprite);
   }
   create() {
+    const isNight = new Date().getHours() > 20 || new Date().getHours() < 6;
     for (let y = 0; y < 10; y++) for (let x = 0; x < 10; x++) {
-      const color = mapData[y][x] === 0 ? 0x228B22 : 0x8B4513;
+      const color = mapData[y][x] === 0 ? (isNight ? 0x1a0033 : 0x228B22) : 0x8B4513;
       this.add.rectangle(x*32+16, y*32+16, 32, 32, color).setStrokeStyle(3, 0x000000);
     }
     playerSprite = this.add.image(playerGridX*32+16, playerGridY*32+16, 'player');
     playerSprite.setDisplaySize(32, 32);
+    this.stepCounter = 0;
   }
 }
 
@@ -44,8 +45,19 @@ function startOverworld(zone) {
     playerGridX = nx; playerGridY = ny;
     playerSprite.x = nx*32+16; playerSprite.y = ny*32+16;
 
+    window.stepCounter = (window.stepCounter || 0) + 1;
+    if (window.stepCounter % 8 === 0 && Math.random() < 0.6) spawnWorldEvent();
     if (mapData[ny][nx] === 0 && Math.random() < 0.25) spawnBattle();
   };
+}
+
+function spawnWorldEvent() {
+  const events = [
+    "🌟 Появился портал в Разлом! +1 NEXUS-очко",
+    "🦔 Ёжик предлагает дружбу (+10 HP всей партии)",
+    "🔥 Рунический Палач даёт квест"
+  ];
+  alert(events[Math.floor(Math.random()*events.length)]);
 }
 
 function exitOverworld() {
